a37dd096ed7542055a3c789983b7c59c44d88c06
solidoracle <solidoracle17@gmail.com>
2023-03-24 13:34:01 +0100

    init

---
+++ packages/hardhat/contracts/SolidStreaming.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: CC0
+pragma solidity ^0.8.10;
+
+// import { ERC20 } from 'solmate/tokens/ERC20.sol';
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title SolidStreaming
+
+contract SolidStreaming {
+	
+}
