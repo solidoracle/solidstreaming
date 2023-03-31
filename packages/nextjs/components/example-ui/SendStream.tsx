e7551fd483455b5311fbfcc045fc8d5c69caae04
solidoracle <solidoracle17@gmail.com>
2023-03-31 14:48:51 +0200

    udpate

--- packages/nextjs/components/example-ui/SendStream.tsx
+++ packages/nextjs/components/example-ui/SendStream.tsx
@@ -42,14 +42,6 @@ export const SendStream = ({ blocknumber: blockNumber }: SendStreamProps) => {
     setPaymentPerBlock(parsedValue);
   }
 
-  interface ScaffoldContractWriteOptions {
-    contractName: string;
-    functionName: string;
-    args: (string | BigNumber | TimeFrame)[];
-    value: string;
-    onSuccess: () => void;
-  }
-
   const { writeAsync, isLoading } = useScaffoldContractWrite({
     contractName: "SolidStreaming",
     functionName: "start",
